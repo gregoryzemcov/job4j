e7bdae60cca578724c7c92b4a4d8bd5798ea41e1
GrigoriyZemtsov <griffondark@gmail.com>
2019-03-07 10:05:50 +0300

    1.2. Подключить сборщик Maven[#4086]

---
+++ chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
@@ -0,0 +1,11 @@
+package ru.job4j.calculate;
+/**
+ * Программа вывода надписи на экран
+ * @autor Gregory Zemtsov
+ * @version 02 27/02/2019
+ */
+public class Calculate {
+    public static void main(String[] args) {
+		System.out.println("Hello World!");
+	}
+}
