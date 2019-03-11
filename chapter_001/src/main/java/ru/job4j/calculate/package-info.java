11087bf0c265f20563acaa044e5350658bbe88ce
gregoryzemcov <griffondark@mail.ru>
2019-03-11 18:06:55 +0300

    1.3 JavaDoc. Code convention [#114645]

---
+++ chapter_001/src/main/java/ru/job4j/calculate/package-info.java
@@ -0,0 +1,7 @@
+/**
+ * Package for calculate task
+ *
+ * @autor Gregory Zemtsov (griffondark@gmail.com)
+ * @version $Id$
+ * @since 0.1
+ */
---
+++ src/ru/job4j/calculate/Calculate.java
@@ -0,0 +1,16 @@
+package ru.job4j.calculate;
+/**
+ * Программа вывода на экран надписи "Hello World!"
+ * @autor Gregory Zemtsov
+ * @version 02 27.02.2019
+ */
+ public class Calculate{
+	/**
+	 * Metod main
+	 * @param String string
+	 * @return string
+	 */
+	 public static void main(String[] args) {		 
+		 System.out.println("Hello World!");
+	 }	 
+ }
