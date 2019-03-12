c880dbecbf3e0e0b727d9ee6168fa5650ae2f8d3
gregoryzemcov <griffondark@mail.ru>
2019-03-12 18:46:08 +0300

    1.4 JUnit [#114646]

---
+++ chapter_001/src/test/java/ru/job4j/calculate/CalculateTest.java
@@ -0,0 +1,23 @@
+package ru.job4j.calculate;
+import org.junit.Test;
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+/**
+ * Программа проверки класса Calculate
+ * @autor Gregory Zemtsov (griffondark@gmail.com)
+ * @version 0.1
+ * @since 11.03.2019
+ */
+ public class CalculateTest{
+	/**
+	* Test echo.
+	*/
+	@Test
+	public void whenTakeNameThenThreeEchoPlusName() {
+		String input = "Gregory Zemtsov";
+		String expect = "Echo, echo, echo : Gregory Zemtsov"; 
+		Calculate calc = new Calculate();
+		String result = calc.echo(input);
+		assertThat(result, is(expect));
+	}
+ }
