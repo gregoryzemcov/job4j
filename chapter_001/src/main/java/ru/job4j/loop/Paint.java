ead8a7837ba74178afa1258f169c35ef36442c1c
gregoryzemcov <griffondark@mail.ru>
2019-03-18 14:17:10 +0300

    5.4 Построить пирамиду в псевдографике. [#114619]

---
+++ chapter_001/src/main/java/ru/job4j/loop/Paint.java
@@ -0,0 +1,27 @@
+package ru.job4j.loop;
+import java.util.function.BiPredicate;
+/**
+ * Программа построения пирамиды в псевдографике.
+ * @author Gregory Zemtsov (griffondark@gmai.com).
+ * @since 18.03.2019.
+ * @version $Id$.
+ */
+ public class Paint {
+	 public String pyramid(int height) {
+		return this.loopBy(height, 2 * height - 1, (row, column) -> row >= height - column - 1 && row + height - 1 >= column);
+	}
+	private String loopBy(int height, int weight, BiPredicate<Integer, Integer> predict) {
+		StringBuilder screen = new StringBuilder();
+		for (int row = 0; row != height; row++) {
+			for (int column = 0; column != weight; column++) {
+				if (predict.test(row, column)) {
+					screen.append("^");
+				} else {
+					screen.append(" ");
+				}
+			}
+			screen.append(System.lineSeparator());
+		}
+		return screen.toString();
+	}
+ }
---
+++ chapter_001/src/test/java/ru/job4j/loop/PaintTest.java
@@ -0,0 +1,42 @@
+package ru.job4j.loop;
+import org.junit.Test;
+import java.util.StringJoiner;
+import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.*;
+/**
+ * Проверка класса Paint
+ * @author Gregory Zemtsov (griffondark@gmail.com).
+ * @version $Id$.
+ * @since 18.03.2019.
+ */
+public class PaintTest {
+    @Test
+    public void whenPyramid4() {
+        Paint paint = new Paint();
+        String rst = paint.pyramid(4);
+        System.out.println(rst);
+        assertThat(rst, is(new StringJoiner(System.lineSeparator(), "", System.lineSeparator())
+                                .add("   ^   ")
+                                .add("  ^^^  ")
+                                .add(" ^^^^^ ")
+                                .add("^^^^^^^")
+                                .toString()
+                )
+        );
+    }
+	@Test
+	public void whenPyramid5() {
+		Paint paint = new Paint();
+		String rst = paint.pyramid(5);
+		System.out.println(rst);
+		assertThat(rst, is(new StringJoiner(System.lineSeparator(), "", System.lineSeparator())
+						.add("    ^    ")
+						.add("   ^^^   ")
+						.add("  ^^^^^  ")
+						.add(" ^^^^^^^ ")
+						.add("^^^^^^^^^")
+						.toString()
+				)
+		);
+	}
+}
