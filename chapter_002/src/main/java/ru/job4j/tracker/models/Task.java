bfacdff80ab3c7d2325f391060f5eaf85c11d470
gregoryzemcov <griffondark@mail.ru>
2019-05-10 16:05:07 +0300

    Часть 002. 2.1 Реализация профессий в коде [114702][#140200]

---
+++ chapter_002/src/main/java/ru/job4j/tracker/models/Task.java
@@ -0,0 +1,16 @@
+package ru.job4j.tracker.models;
+/**
+ * Шаблон класса "Задача", унаследован от Item.
+ * @author Gregory Zemtsov (griffondark@gmail.com).
+ * @version $Id$
+ * @since 05.05.2019.
+ */ 
+ public class Task extends Item {
+	 public Task(String name, String desc) {
+		 this.name = name;
+		 this.description = desc;
+	 }	 
+	 public String calculatePrice() {
+		 return "100%";
+	 }
+ }
---
+++ chapter_002/src/main/java/ru/job4j/tracker/profession/Student.java
@@ -0,0 +1,13 @@
+package ru.job4j.tracker.profession;
+/**
+ * Класс Student, наследуемый от класса Teacher.
+ * @author Gregory Zemtsov (griffondark@gmail.com).
+ * @version $Id$.
+ * @since 06.05.2019.
+ */
+ public class Student extends Teacher {
+	 private String teach;
+	 public String teach() {
+		 return teach;
+	 }
+ }
