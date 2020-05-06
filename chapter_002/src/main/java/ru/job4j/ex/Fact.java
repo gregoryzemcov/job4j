1189cc0afb078e82e40a2aafded2c86895a02314
gregoryzemcov <griffondark@mail.ru>
2020-05-06 22:42:49 +0300

    2.5.0.3. Тестирование исключений с junit [#219366]

--- chapter_002/src/main/java/ru/job4j/ex/Fact.java
+++ chapter_002/src/main/java/ru/job4j/ex/Fact.java
@@ -1,5 +1,10 @@
 package ru.job4j.ex;
-
+/**
+ * Класс расчета факториала заданного числа.
+ * @author Gregory Zemtsov (griffondark@gmai.com).
+ * @since 30.04.2020.
+ * @version $Id$.
+ */
 public class Fact {
     public static void main(String[] args) {
         System.out.println("Значение факториала введенного числа:" + calc(5));
